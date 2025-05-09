5f927a45f8ea79119a1d7c32d8a36e0150fdcc55
Vedanssh <vedutrivediusa@gmail.com>
2025-05-09 10:08:27 +0530

    minor login bug solved

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -388,6 +388,7 @@ const LoginScreen = ({ navigation }) => {
             }
             keyboardType="phone-pad"
             onChangeText={onChangeText}
+            maxLength={10}
           />
 
           <View
